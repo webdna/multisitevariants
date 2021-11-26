c929409c0c34f4b93081e844aa3bb764632bf89d
Mike <mike@webdna.co.uk>
2021-11-26 16:49:09 +0000

    fix broken variant inputs

--- src/assetbundles/multisitevariants/dist/js/multisite-variants.js
+++ src/assetbundles/multisitevariants/dist/js/multisite-variants.js
@@ -18,9 +18,28 @@
          $multiFields = document.querySelector(`#variants-${ variantId }-multi-site`).children;
      Array.from($multiFields).forEach(el => {
          $metaFields.appendChild(el);
+         if (el.id.indexOf('stock') !== -1) {
+
+            //  lets instantiate the stock fields
+            let checkbox = el.querySelector('input.unlimited-stock.checkbox');
+            checkbox.addEventListener("change", function(i) {
+                let input = el.querySelector('.textwrapper input');
+                console.log(input);
+                if (checkbox.checked == true) {
+                    input.disabled = true;
+                    input.classList.add('disabled');
+                    // input.value = '';
+                } else {
+                    input.disabled = false;
+                    input.classList.remove('disabled');
+                }
+            }); 
+         }
+
      });
  
  });
  document.querySelector('#variant-multi-site').remove();
+
  
  
